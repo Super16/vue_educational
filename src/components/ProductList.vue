5e9cd5e0c51aa7d9e5f001768eef5ad1395fb2fe
mark <super16mail@gmail.com>
2020-10-05 23:08:48 +0300

    Added ProductItem and ProductList

---
+++ src/components/ProductList.vue
@@ -0,0 +1,12 @@
+<template>
+  <ProductItem :products="products"/>
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue';
+
+export default {
+  props: ['products'],
+  components: { ProductItem },
+};
+</script>
