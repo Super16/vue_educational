1420d0863bd24fbf45d9a857fb85b8f6107ca344
mark <super16mail@gmail.com>
2020-10-12 23:09:00 +0300

    Added filter options

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -10,27 +10,10 @@
     <span class="catalog__price">{{ product.price }} ₽</span>
 
     <ul class="colors colors--black">
-      <li class="colors__item">
+      <li class="colors__item" v-for="color in productColors(product.colors)" :key="color">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio"
-          value="#73B6EA" v-model="color">
-          <span class="colors__value" style="background-color: #73B6EA;">
-          </span>
-        </label>
-      </li>
-      <li class="colors__item">
-        <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio"
-          value="#8BE000" v-model="color">
-            <span class="colors__value" style="background-color: #8BE000;">
-            </span>
-        </label>
-      </li>
-      <li class="colors__item">
-        <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio"
-          value="#222" v-model="color">
-          <span class="colors__value" style="background-color: #222;">
+          <input class="colors__radio sr-only" type="radio" :value="color.code">
+          <span class="colors__value" :style="{ backgroundColor: color.code, }">
           </span>
         </label>
       </li>
@@ -39,14 +22,25 @@
 </template>
 
 <script>
+import colors from '../data/colors';
+
 export default {
-  data() {
-    return {
-      color: '#73B6EA',
-    };
+  computed: {
+    colors() {
+      return colors;
+    },
   },
   props: {
     product: Object,
+  },
+  methods: {
+    productColors(value) {
+      let productColors = colors;
+
+      productColors = productColors.filter((item) => value.includes(item.id));
+
+      return productColors;
+    },
   },
 };
 </script>
---
+++ src/data/colors.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    code: '#FFBE15',
+  },
+  {
+    id: 2,
+    code: '#939393',
+  },
+  {
+    id: 3,
+    code: '#8BE000',
+  },
+  {
+    id: 4,
+    code: '#FF6B00',
+  },
+  {
+    id: 5,
+    code: '#394939',
+  },
+  {
+    id: 6,
+    code: '#000',
+  },
+  {
+    id: 7,
+    code: '#73B6EA',
+  },
+];
